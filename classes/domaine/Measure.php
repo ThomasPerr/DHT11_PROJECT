584ae2893e1f55b01177ba0c19194a5404ea7eb3
thomas <thomas.perret43@gmail.com>
2018-02-01 15:20:33 +0100

    a jour

---
+++ classes/domaine/Measure.php
@@ -0,0 +1,14 @@
+<?php
+namespace domaine;
+
+class Measure
+{
+    public $Temperature;
+    public $Humidite;
+    
+    public function __construct($Temperature, $Humidite){
+        $this->Temperature = $UserID;
+        $this->Humidite = $Nom;  
+    }
+}
+
