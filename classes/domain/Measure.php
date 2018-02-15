969904739292184c3a4f4299d9275d5dbdfba6fe
thomas <thomas.perret43@gmail.com>
2018-02-15 13:59:48 +0100

    yes

---
+++ classes/domain/Measure.php
@@ -0,0 +1,15 @@
+<?php
+namespace domain;
+
+class Measure
+{
+    public $temperature;
+    public $humidite;
+
+    public function __construct($ID, $temperature, $humidite){
+        $this->ID = $ID;
+        $this->temperature = $temperature;
+        $this->humidite = $humidite;
+
+    }
+}
